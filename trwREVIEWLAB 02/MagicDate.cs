bc8f00ea410afd65f374c019ef72d757f499f80f
T. Robert Ward <trironyard@icloud.com>
2016-10-02 15:12:24 -0400

    initial commit

---
+++ trwREVIEWLAB 02/MagicDate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trwREVIEWLAB_02
+{
+    class MagicDate
+    {
+        private int mon, dy, yr;
+        private bool isMagic = false;
+
+        public int Month
+        {
+            set
+            {
+                mon = value;
+            }
+
+            get
+            {
+                return mon;
+            }
+        }
+
+        public int Day
+        {
+            set
+            {
+                dy = value;
+            }
+
+            get
+            {
+                return dy;
+            }
+        }
+
+        public int Year
+        {
+            set
+            {
+                yr = value;
+            }
+
+            get
+            {
+                return yr;
+            }
+        }
+
+        public bool DetermineIfMagic()
+        {
+            if (dy*mon == yr)
+                isMagic = true;
+            return isMagic;
+        }
+    }
+}
---
+++ trwREVIEWLAB 02/Program.cs
@@ -0,0 +1,42 @@
+﻿//T. Robert Ward    8/24/14
+//Assignment # 1-2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trwREVIEWLAB_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            //int day, month, year;
+            MagicDate checkToSeeIfMagic = new MagicDate();
+            
+            Console.WriteLine("*****  MagicDate  *****\n\n");
+            Console.Write("Please enter in the Day - [ 1 through 31 ]: ");
+            input = Console.ReadLine();
+            checkToSeeIfMagic.Day = int.Parse(input);
+
+            Console.Write("Please enter in the Month - [ 1 through 12 ]: ");
+            input = Console.ReadLine();
+            checkToSeeIfMagic.Month = int.Parse(input);
+
+            Console.Write("Please enter in the Year - [ 00 through 99 ]: ");
+            input = Console.ReadLine();
+            checkToSeeIfMagic.Year = int.Parse(input);
+
+            if ( checkToSeeIfMagic.DetermineIfMagic() == true )
+                Console.WriteLine("This is a Magic Date !!!!");
+            else
+                Console.WriteLine("This is not a magic date.");
+
+            Console.Write("Press Enter Key");
+            input = Console.ReadLine();
+        }
+    }
+}
